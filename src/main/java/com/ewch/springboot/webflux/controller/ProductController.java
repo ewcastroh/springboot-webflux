2073663212bf1607629f0255545b52d3317d00af
eimer-castro-g <eimer.castro@globant.com>
2020-10-02 13:59:13 -0500

    Implementing Reactive Data Driven to improve backpressure.

--- src/main/java/com/ewch/springboot/webflux/controller/ProductController.java
+++ src/main/java/com/ewch/springboot/webflux/controller/ProductController.java
@@ -2,11 +2,13 @@ package com.ewch.springboot.webflux.controller;
 
 import com.ewch.springboot.webflux.dao.ProductDao;
 import com.ewch.springboot.webflux.model.document.Product;
+import java.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
 
 @Controller
@@ -30,6 +32,21 @@ public class ProductController {
 		productFlux.subscribe(product -> LOGGER.info(product.getName()));
 
 		model.addAttribute("products", productFlux);
+		model.addAttribute("title", "products list");
+		return "productList";
+	}
+
+	@GetMapping("/products-datadriver")
+	public String getProductsDataDriver(Model model) {
+		Flux<Product> productFlux = productDao.findAll()
+			.map(product -> {
+				product.setName(product.getName().toUpperCase());
+				return product;
+			})
+			.delayElements(Duration.ofSeconds(1));
+		productFlux.subscribe(product -> LOGGER.info(product.getName()));
+
+		model.addAttribute("products", new ReactiveDataDriverContextVariable(productFlux, 1));
 		model.addAttribute("title", "products list");
 		return "productList";
 	}
