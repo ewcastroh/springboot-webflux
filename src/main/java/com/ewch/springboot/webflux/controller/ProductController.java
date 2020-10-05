cf9c8010889f6aec1d5ce08d681282bedeada18f
eimer-castro-g <eimer.castro@globant.com>
2020-10-05 08:24:19 -0500

    Adding handler methods to create and save products.

--- src/main/java/com/ewch/springboot/webflux/controller/ProductController.java
+++ src/main/java/com/ewch/springboot/webflux/controller/ProductController.java
@@ -8,8 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class ProductController {
@@ -23,38 +25,55 @@ public class ProductController {
 	}
 
 	@GetMapping({"/products", "/"})
-	public String getProducts(Model model) {
+	public Mono<String> getProducts(Model model) {
 		Flux<Product> productFlux = productService.findAllWithUpperCaseName();
 		productFlux.subscribe(product -> LOGGER.info(product.getName()));
 		model.addAttribute("products", productFlux);
 		model.addAttribute("title", "Products list");
-		return "productList";
+		return Mono.just("productList");
+	}
+
+	@GetMapping("/form")
+	public Mono<String> createProduct(Model model) {
+		model.addAttribute("product", new Product());
+		model.addAttribute("title", "Product Form");
+		return Mono.just("form");
+	}
+
+	@PostMapping("/form")
+	public Mono<String> saveProduct(Product product) {
+		return productService.save(product)
+			.doOnNext(product1 -> {
+				LOGGER.info("Saved product: " + product.toString());
+			})
+			.thenReturn("redirect:/productList");
+			//.then(Mono.just("redirect:/productList"));
 	}
 
 	@GetMapping("/products-datadriver")
-	public String getProductsDataDriver(Model model) {
+	public Mono<String> getProductsDataDriver(Model model) {
 		Flux<Product> productFlux = productService.findAllWithUpperCaseName()
 			.delayElements(Duration.ofSeconds(1));
 		productFlux.subscribe(product -> LOGGER.info(product.getName()));
 		model.addAttribute("products", new ReactiveDataDriverContextVariable(productFlux, 1));
 		model.addAttribute("title", "Products list");
-		return "productList";
+		return Mono.just("productList");
 	}
 
 	@GetMapping("/products-full")
-	public String getProductsFull(Model model) {
+	public Mono<String> getProductsFull(Model model) {
 		Flux<Product> productFlux = productService.findAllWithUpperCaseNameRepeat();
 		model.addAttribute("products", productFlux);
 		model.addAttribute("title", "Products list");
-		return "productList";
+		return Mono.just("productList");
 	}
 
 	@GetMapping("/products-chunked")
-	public String getProductsChunked(Model model) {
+	public Mono<String> getProductsChunked(Model model) {
 		Flux<Product> productFlux = productService.findAllWithUpperCaseNameRepeat();
 		model.addAttribute("products", productFlux);
 		model.addAttribute("title", "Products list");
-		return "productList-chunked";
+		return Mono.just("productList-chunked");
 	}
 
 }
